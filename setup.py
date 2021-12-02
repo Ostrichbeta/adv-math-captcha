e928a74a12e99ed3da0b4717d7c0b17afdd92ce0
Ostrichbeta <ostrichb@outlook.com>
2021-12-02 04:00:27 +0000

    make the first build

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="adv-math-captcha-ostrichb",
+    version="0.9.9",
+    author="Ostrichbeta",
+    author_email="ostrichb@yandex.com",
+    description="Advanced mathematical captcha generator and ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ostrichbeta/adv-math-captcha",
+    project_urls={
+        "Bug Tracker": "https://github.com/Ostrichbeta/adv-math-captcha/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 4",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "sympy>=1.9"
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.8",
+)
